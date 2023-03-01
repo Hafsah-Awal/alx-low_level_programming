61cc1be874621b0670c9052001bc508c40bb5094
Hafsah-Awal <awal.hafsat2000@gmail.com>
2023-03-01 10:19:11 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[j] - src[j];
+		dest[j] = src[j];
 		j++;
 	}
 	while (j < n)
 	{
-		dest[j] - '\0';
+		dest[j] = '\0';
 		j++;
 	}
 	return (dest);

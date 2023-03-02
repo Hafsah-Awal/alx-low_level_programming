f5ed05c55197c2aa7ac39076350496b17ad76d92
Hafsah-Awal <awal.hafsat2000@gmail.com>
2023-03-02 04:02:13 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,14 +10,15 @@ void print_number(int n)
 	unsigned int n1;
 
 	n1 = n;
+
 	if (n < 0)
 	{
 		_putchar('_');
-		n1 = -n;
+		n1 =  -n;
 	}
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	_putchar(n1 % 10 + '0');
 }
